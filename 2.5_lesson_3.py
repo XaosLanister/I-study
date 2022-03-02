442085c4395d97658c3fbd617287957d962a7e34
XaosLanister <vitaltron22@gmail.com>
2022-03-02 20:50:11 +0300

    module 'collection'

---
+++ 2.5_lesson_3.py
@@ -0,0 +1,17 @@
+import collections
+from typing import Counter
+
+
+meanings = [int(i) for i in input().split()]
+
+a = Counter(meanings).most_common()
+c = ''
+
+for i in a:
+    if i[1] == 1:
+        pass
+    else:
+        c += str(i[0]) + ' '
+print(c)
+
+"""Today I meet module 'collection'!!! It's realy cool module)"""
