a5647f71dd0e4c4f728378e15ebcdf3eddb07638
XaosLanister <vitaltron22@gmail.com>
2022-03-03 22:41:54 +0300

    v.2

--- 2.6_weekly_report_1.py
+++ 2.6_weekly_report_1.py
@@ -1,13 +1,11 @@
-sum_meanings = 0
-sum_square = 0
+sum_meanings, sum_square = 0, 0
 
 while True:
     meanings = int(input())
     if meanings != 0:
         sum_meanings += meanings
-        if sum_meanings != 0:
-            sum_square += meanings ** 2
-        if sum_meanings <= 0:
-            break
+        sum_square += pow(meanings, 2)
+    if sum_meanings == 0:
+        break
 print(sum_square)
     
