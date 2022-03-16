4cb11ee3ecbb4ccdfb7fd11a31bb3bbdf60119a8
XaosLanister <vitaltron22@gmail.com>
2022-03-16 20:20:23 +0300

    v 1.0

---
+++ 2.6_weekly_report_4.py
@@ -0,0 +1,10 @@
+meaning, date, column = input(), [], 0
+
+while meaning != 'end':
+    date += [[int(i) for i in meaning.split()]]
+    meaning = input()
+    column += 1
+#print (date)
+#print(count)
+string = len(date[0])
+#print(string)
