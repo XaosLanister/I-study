bc3c771e2ca354161edb72762ab70aa0cff4f1c1
XaosLanister <vitaltron22@gmail.com>
2022-03-19 00:14:05 +0300

    v 1.2

--- 2.6_weekly_report_5.py
+++ 2.6_weekly_report_5.py
@@ -1,8 +1,32 @@
 meaning = int(input())
-c = 0
+count = meaning
 
 a = [[0] * meaning for i in range(meaning)]
-a[0] = [(col + 1) for col in range(meaning)]
+a[0] = [(i + 1) for i in range(meaning)]
+
+st, col, cycle_length  = 0, meaning - 1, meaning
+
+while count < meaning * meaning:
+    cycle_length -= 1
+    for i in range(cycle_length):
+        st += 1
+        count += 1
+        a[st][col] = count
+    for i in range(cycle_length):
+        col -= 1
+        count += 1
+        a[st][col] = count   
+    cycle_length -= 1
+    for i in range(cycle_length):
+        st -= 1
+        count += 1
+        a[st][col] = count
+    for i in range(cycle_length):
+        col += 1
+        count += 1
+        a[st][col] = count
+    if count >= meaning * meaning:
+        break
 
 for i in a:
-    print(i)
+    print(*i)
