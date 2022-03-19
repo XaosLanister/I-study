ae707c948b52e26c7372886beb9ea0edf6f147ed
XaosLanister <vitaltron22@gmail.com>
2022-03-19 09:55:30 +0300

    v 1.3

--- 2.6_weekly_report_5.py
+++ 2.6_weekly_report_5.py
@@ -6,25 +6,26 @@ a[0] = [(i + 1) for i in range(meaning)]
 
 st, col, cycle_length  = 0, meaning - 1, meaning
 
+def all_count():
+    global count
+    count += 1
+    a[st][col] = count
+
 while count < meaning * meaning:
     cycle_length -= 1
     for i in range(cycle_length):
         st += 1
-        count += 1
-        a[st][col] = count
+        all_count()
     for i in range(cycle_length):
         col -= 1
-        count += 1
-        a[st][col] = count   
+        all_count()
     cycle_length -= 1
     for i in range(cycle_length):
         st -= 1
-        count += 1
-        a[st][col] = count
+        all_count()
     for i in range(cycle_length):
         col += 1
-        count += 1
-        a[st][col] = count
+        all_count()
     if count >= meaning * meaning:
         break
 
