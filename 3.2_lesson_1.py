e1dcdf0ba91f4ce9bc6bdffd1e869c4d9edaa53a
XaosLanister <vitaltron22@gmail.com>
2022-03-20 19:39:02 +0300

    v 1.2

--- 3.2_lesson_1.py
+++ 3.2_lesson_1.py
@@ -1,19 +1,18 @@
-from turtle import up
-
-
 d = {}
 
 
 def update_dictionary(d, key, value):
+    if key not in d:
+        key = 2 * key
     if key in d:
         d[key] += [value]
-    else:
-        key = 2 * key
+    if key not in d:
         d.setdefault(key)
         d[key] = [value]
         d.update
-        
+
+
 update_dictionary(d, 1, -1)
 update_dictionary(d, 2, -2)
-#update_dictionary(d, 1, -3)
+update_dictionary(d, 1, -3)
 print(d)
