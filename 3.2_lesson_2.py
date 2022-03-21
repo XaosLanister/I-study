04159f8f0433f0f61b6d6bd60136fe0d31d2e551
XaosLanister <vitaltron22@gmail.com>
2022-03-21 19:36:43 +0300

    v 1.4

--- 3.2_lesson_2.py
+++ 3.2_lesson_2.py
@@ -3,9 +3,11 @@ from collections import Counter
 
 word = input().lower().split()
 
+
 def counts(word):
     f = dict(Counter(word))
-    for i in f:
-        print(f)
-        
+    for key,value in f.items():
+        print(key, value)
+
+
 counts(word)
