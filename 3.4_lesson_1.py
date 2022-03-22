aeaa21a943944de14ada6e9ecc207368f4da6d42
XaosLanister <vitaltron22@gmail.com>
2022-03-22 22:40:16 +0300

    v 1.4

--- 3.4_lesson_1.py
+++ 3.4_lesson_1.py
@@ -1,6 +1,34 @@
 import re
 
+
+def main(name):
+    dataSet = open(name)
+
+    file_read = re.split('(\d+)', dataSet.read())
+
+    count = 0
+    textOut = ''
+    while count < len(file_read):
+        if file_read[count] == '':
+            break
+        else:
+            textOut += str(file_read[count] * int(file_read[count + 1]))
+            count += 2
+    fileOut = open('result.txt', 'w')
+    for i in textOut:
+        fileOut.write(i)
+    fileOut.close
+
+main('test.txt')
+
+
+
+
+
+""" import re
+
 dataSet = open('test.txt')
+
 file_read = re.split('(\d+)', dataSet.read())
 
 count = 0
@@ -11,4 +39,4 @@ while count < len(file_read):
     else:
         s += str(file_read[count] * int(file_read[count + 1]))
         count += 2
-print(s)
+print(s) """
