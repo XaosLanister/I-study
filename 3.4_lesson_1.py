8499234391d7a1970129e57f491b9ba91cd51e56
XaosLanister <vitaltron22@gmail.com>
2022-03-22 21:13:18 +0300

    v 1.3

--- 3.4_lesson_1.py
+++ 3.4_lesson_1.py
@@ -2,10 +2,13 @@ import re
 
 dataSet = open('test.txt')
 file_read = re.split('(\d+)', dataSet.read())
-print(file_read)
+
 count = 0
 s = ''
 while count < len(file_read):
-    s += str(file_read[count] * int(file_read[count + 1]))
-    count += 2
-    print(s)
+    if file_read[count] == '':
+        break
+    else:
+        s += str(file_read[count] * int(file_read[count + 1]))
+        count += 2
+print(s)
