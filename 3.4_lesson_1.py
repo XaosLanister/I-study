a0108e00202a6fbee92844d2ff58324df0d9378d
XaosLanister <vitaltron22@gmail.com>
2022-03-22 23:23:30 +0300

    v 1.6

--- 3.4_lesson_1.py
+++ 3.4_lesson_1.py
@@ -4,17 +4,22 @@ import re
 def main(name):
     dataSet = open(name)
     file_read = re.split('(\d+)', dataSet.read())
+    dataSet.close
+    
     count = 0
     textOut = ''
+    
     while count < len(file_read):
-        if file_read[count] == '':
+        if file_read[count] == '\n':
             break
         else:
             textOut += str(file_read[count] * int(file_read[count + 1]))
             count += 2
+
     fileOut = open('result.txt', 'w')
+
     for i in textOut:
         fileOut.write(i)
     fileOut.close
 
-main('test.txt')
+main('dataset_3363_2.txt')
