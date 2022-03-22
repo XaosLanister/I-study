f6de34d188484cbcef19f7497074c60740903c4b
XaosLanister <vitaltron22@gmail.com>
2022-03-22 20:43:51 +0300

    v 1.1

--- 3.4_lesson_1.py
+++ 3.4_lesson_1.py
@@ -1,5 +1,14 @@
-outMeaning, outMeaningWrite = '', ''
+import re
 
 dataSet = open('test.txt')
-file_read = dataSet.read()
+file_read = re.split('(\d+)', dataSet.read())
+
+counter = 0
+for i in file_read:
+    if counter < len(file_read):
+        result = i[counter] * i[counter + 1]
+    else:
+        break
+print(result)
+
 
