06a3952ae9206aefdd8384d949239ac021b2c4e4
XaosLanister <vitaltron22@gmail.com>
2022-03-22 22:55:43 +0300

    v 1.5

--- 3.4_lesson_1.py
+++ 3.4_lesson_1.py
@@ -3,9 +3,7 @@ import re
 
 def main(name):
     dataSet = open(name)
-
     file_read = re.split('(\d+)', dataSet.read())
-
     count = 0
     textOut = ''
     while count < len(file_read):
@@ -20,23 +18,3 @@ def main(name):
     fileOut.close
 
 main('test.txt')
-
-
-
-
-
-""" import re
-
-dataSet = open('test.txt')
-
-file_read = re.split('(\d+)', dataSet.read())
-
-count = 0
-s = ''
-while count < len(file_read):
-    if file_read[count] == '':
-        break
-    else:
-        s += str(file_read[count] * int(file_read[count + 1]))
-        count += 2
-print(s) """
