7f848c718aecb480e4651c823fa32e42f6105e43
XaosLanister <vitaltron22@gmail.com>
2022-03-23 18:13:56 +0300

    v 1.1

--- 3.4_lesson_2.py
+++ 3.4_lesson_2.py
@@ -1,12 +1,13 @@
 import collections
-from typing import Counter
+from collections import Counter
 
 def main(name):
     dataSet = open(name)
-    fileText = dataSet.read().lower()
+    fileText = Counter((dataSet.read().lower()).split()).most_common
     dataSet.close
+    #fileText = Counter(fileText.split(' ')).most_common
+    #fileText = Counter(fileText).most_common
     print(fileText)
-
 
 
 main('test.txt')
