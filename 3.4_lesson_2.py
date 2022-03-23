7e275d6fb9a6c920fddaf0df9ab9780fb3e4a3c8
XaosLanister <vitaltron22@gmail.com>
2022-03-23 21:37:40 +0300

    v 1.3

--- 3.4_lesson_2.py
+++ 3.4_lesson_2.py
@@ -1,13 +1,23 @@
 import collections
 from collections import Counter
 
+
 def main(name):
     dataSet = open(name)
-    fileText = Counter((dataSet.read().lower()).split()).most_common
+    fileText = Counter((dataSet.read().lower()).split()).most_common()
+    fileText.sort()
     dataSet.close
-    #fileText = Counter(fileText.split(' ')).most_common
-    #fileText = Counter(fileText).most_common
     print(fileText)
-
-
+    dic = []
+    for i in fileText:
+        if fileText[0][1] == i[1]:
+            dic += i
+    print(*dic)
 main('test.txt')
+
+
+
+
+"""             dic += i
+        else:
+            break    """
