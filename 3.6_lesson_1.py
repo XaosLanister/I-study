6917ddf0624092e7f569cb5d93f794dd81f64fda
XaosLanister <vitaltron22@gmail.com>
2022-03-26 22:52:06 +0300

    v 1.2

---
+++ 3.6_lesson_1.py
@@ -0,0 +1,11 @@
+import requests
+
+
+with open('dataset_3378_2.txt') as link:
+    url = link.read().strip('\n')
+
+with open('658.txt', 'a+') as text:
+    url = requests.get(url)
+    st = url.text
+    #text.write(st)
+    text.write(str(len(st.splitlines())))
