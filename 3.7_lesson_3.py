dc9a15edbec04334ca92264a651f3c032e0c0e71
XaosLanister <vitaltron22@gmail.com>
2022-03-27 21:48:14 +0300

    v 1.6

--- 3.7_lesson_3.py
+++ 3.7_lesson_3.py
@@ -1,14 +1,17 @@
 count_word = int(input())
-count_str= int(input())
 
 def my_word(count_word):
-    date = []
+    date_word = []
     for i in range(count_word):
         i = input().lower()
-        date.append(i)
-        #print(date)
-    return date
-my_word(count_word)
+        date_word.append(i)
+    #print(date_word)
+    return date_word
+date_word = my_word(count_word)
+
+
+count_str= int(input())
+
 
 def my_str(count_str):
     date_str = []
@@ -16,4 +19,11 @@ def my_str(count_str):
         i = input().lower().split()
         date_str += i
     #print(date_str)
-my_str(count_str)
+    return date_str
+date_str = my_str(count_str)
+
+
+def check(date_word, date_str):
+    for i in date_word:
+        print(i)
+check(date_word, date_str)
