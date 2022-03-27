5b8824fff335399699572ecdf2dfd9cc2a80474d
XaosLanister <vitaltron22@gmail.com>
2022-03-27 15:12:29 +0300

    v 1.6

--- 3.6_lesson_2.py
+++ 3.6_lesson_2.py
@@ -6,10 +6,10 @@ with open('dataset_3378_3.txt') as file:
     file_name = file.readline().strip()
 file_name = str(requests.get(file_name).text)
 
-#counts = 1
+counts = 1    #Счетчик для того что бы был виден процесс
 
 while 'we'not in file_name:
-    #print(file_name, counts)
+    print(file_name, counts)        #принтует ход выполнения цикла
     file_name = requests.get(file_link + file_name).text
-    #counts += 1
-print(file_name, file= open('result.txt', 'w'))
+    counts += 1
+print(file_name, file= open('result.txt', 'w'))                 #запись результата в файл.
