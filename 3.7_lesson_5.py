d5d70ff0d44e21b9313b26faef776263dba13f8a
XaosLanister <vitaltron22@gmail.com>
2022-03-28 22:42:44 +0300

    v 1.2

--- 3.7_lesson_5.py
+++ 3.7_lesson_5.py
@@ -1,17 +1,15 @@
 average_height ={
-    1: [], 2: [], 3: [], 4: [],
-    5: [], 6: [], 7: [], 8: [],
-    9: [], 10: [], 11: [],
+    1: [], 2: [], 3: [], 4: [],5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [],
 }
+
 with open('dataset_3380_5.txt') as _:
     for lines in _:
         lines = lines.strip().split()
         line = int(lines[0])
         if line in average_height.keys():
-            average_height[line].append(lines[1])
+            #average_height[line].append(lines[1])
             average_height[line].append(int(lines[2]))
-print(average_height)
-    
-    
-    
+
+    for i in average_height.values():
+        print(sum(i) / len(i))
     
