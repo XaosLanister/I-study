c45522a96a63d4389f8c2173ad16ad9d1355bee6
XaosLanister <vitaltron22@gmail.com>
2022-03-28 21:14:47 +0300

    v 1.1

--- 3.7_lesson_5.py
+++ 3.7_lesson_5.py
@@ -1,17 +1,16 @@
 average_height ={
-    1: None, 2: None, 3: None, 4: None,
-    5: None, 6: None, 7: None, 8: None,
-    9: None, 10: None, 11: None,
+    1: [], 2: [], 3: [], 4: [],
+    5: [], 6: [], 7: [], 8: [],
+    9: [], 10: [], 11: [],
 }
 with open('dataset_3380_5.txt') as _:
     for lines in _:
         lines = lines.strip().split()
-        #print(lines)
-        for line in lines:
-            #print(lines[0])
-"""             if int(line[0]) == average_height.keys():
-                average_height.update(line[1],line[2])
-                print(average_height) """
+        line = int(lines[0])
+        if line in average_height.keys():
+            average_height[line].append(lines[1])
+            average_height[line].append(int(lines[2]))
+print(average_height)
     
     
     
